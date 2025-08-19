b8c9e1a4987bcaaf561a98c126b7acbc175eaa74
박종하 <joshua_0621@naver.com>
2025-08-19 14:31:58 +0900

    Initial commit

---
+++ src/main/kotlin/kr/co/archan/reflect/ReflectBeApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.archan.reflect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReflectBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReflectBeApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/archan/reflect/ReflectBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.archan.reflect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReflectBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
