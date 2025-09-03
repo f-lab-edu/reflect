316b0963f068ff93ab450bd8804aae8b302c4d4a
박종하 <joshua_0621@naver.com>
2025-09-03 12:04:42 +0900

    [FIX] NoMemberException 응답 코드 BadRequest로 수정

--- src/main/kotlin/kr/co/archan/reflect/member/exception/NoMemberException.kt
+++ src/main/kotlin/kr/co/archan/reflect/member/exception/NoMemberException.kt
@@ -4,5 +4,5 @@ import kr.co.archan.reflect.global.exception.ProductException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 class NoMemberException : ProductException("회원이 없습니다.")
