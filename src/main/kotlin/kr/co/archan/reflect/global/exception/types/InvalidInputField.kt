b1a65fb56cfca470d5f4895a61e2dabd67ccc051
박종하 <joshua_0621@naver.com>
2025-09-08 17:43:09 +0900

    [FEAT] 잘못 입력된 필드에 대한 인터페이스 생성

---
+++ src/main/kotlin/kr/co/archan/reflect/global/exception/types/InvalidInputField.kt
@@ -0,0 +1,9 @@
+package kr.co.archan.reflect.global.exception.types
+
+// 잘못된 입력에 대한 예외는 모든 도메인에서 사용 가능하기 때문에
+// 잘못 입력된 필드의 정보를 추상화한다
+
+interface InvalidInputField {
+    val field: String
+    val message: String
+}
