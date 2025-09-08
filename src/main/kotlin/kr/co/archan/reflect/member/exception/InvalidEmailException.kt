f65c86100fabe6e2f9c08c58662fd8874f8da0ce
박종하 <joshua_0621@naver.com>
2025-09-08 18:01:25 +0900

    [REMOVE] 기존 Invalid 예외 삭제

--- src/main/kotlin/kr/co/archan/reflect/member/exception/InvalidEmailException.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.archan.reflect.member.exception
-
-import kr.co.archan.reflect.global.exception.base.ProductException
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-class InvalidEmailException : ProductException("이메일 입력이 잘못되었습니다")
