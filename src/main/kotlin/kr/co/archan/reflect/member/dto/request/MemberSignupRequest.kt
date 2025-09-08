74eee9e321540e70dad50414af5015d8f107987d
박종하 <joshua_0621@naver.com>
2025-09-08 17:45:05 +0900

    [FIX] 기존 잘못된 입력 예외 새로운 InvalidInputException으로 수정

--- src/main/kotlin/kr/co/archan/reflect/member/dto/request/MemberSignupRequest.kt
+++ src/main/kotlin/kr/co/archan/reflect/member/dto/request/MemberSignupRequest.kt
@@ -1,10 +1,9 @@
 package kr.co.archan.reflect.member.dto.request
 
+import kr.co.archan.reflect.global.exception.common.InvalidInputException
 import kr.co.archan.reflect.global.util.Validator
 import kr.co.archan.reflect.global.util.extension.requireAndThrowProductException
-import kr.co.archan.reflect.member.exception.InvalidEmailException
-import kr.co.archan.reflect.member.exception.InvalidNameException
-import kr.co.archan.reflect.member.exception.InvalidPasswordException
+import kr.co.archan.reflect.member.exception.types.MemberInvalidInputField
 
 data class MemberSignupRequest(
     val email: String,
@@ -12,8 +11,8 @@ data class MemberSignupRequest(
     val name: String
 ) {
     init {
-        requireAndThrowProductException(Validator.isNameValid(name)) { InvalidNameException() }
-        requireAndThrowProductException(Validator.isEmailValid(email)) { InvalidEmailException() }
-        requireAndThrowProductException(Validator.isPasswordValid(password)) { InvalidPasswordException() }
+        requireAndThrowProductException(Validator.isNameValid(name)) { InvalidInputException(MemberInvalidInputField.NAME) }
+        requireAndThrowProductException(Validator.isEmailValid(email)) { InvalidInputException(MemberInvalidInputField.EMAIL) }
+        requireAndThrowProductException(Validator.isPasswordValid(password)) { InvalidInputException(MemberInvalidInputField.PASSWORD) }
     }
 }
