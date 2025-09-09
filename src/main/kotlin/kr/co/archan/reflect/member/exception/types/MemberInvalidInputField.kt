4c362f5d8784655d0737f58522956cb7787c9ffe
박종하 <joshua_0621@naver.com>
2025-09-09 16:13:02 +0900

    [FIX] 패스워드, 이메일 오류 안내 메세지 바뀜 수정

--- src/main/kotlin/kr/co/archan/reflect/member/exception/types/MemberInvalidInputField.kt
+++ src/main/kotlin/kr/co/archan/reflect/member/exception/types/MemberInvalidInputField.kt
@@ -3,7 +3,7 @@ package kr.co.archan.reflect.member.exception.types
 import kr.co.archan.reflect.global.exception.types.InvalidInputField
 
 enum class MemberInvalidInputField (override val field: String, override val message: String): InvalidInputField {
-    EMAIL("email", "패스워드는 8자 이상 64자 이하로, 숫자와 특수문자(공백 제외)를 포함해야 합니다"),
+    EMAIL("email", "이메일 입력이 잘못되었습니다"),
     NAME("name", "이름 형식이 잘못되었습니다"),
-    PASSWORD("password", "이메일 입력이 잘못되었습니다")
+    PASSWORD("password", "패스워드는 8자 이상 64자 이하로, 숫자와 특수문자(공백 제외)를 포함해야 합니다")
 }
