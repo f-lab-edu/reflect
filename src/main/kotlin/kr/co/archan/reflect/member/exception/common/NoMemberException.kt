0b37b4de711ef8f3eadf1e0a5edd4670316538ee
박종하 <joshua_0621@naver.com>
2025-09-09 20:10:40 +0900

    [REMOVE] 미사용 예외 제거

--- src/main/kotlin/kr/co/archan/reflect/member/exception/common/NoMemberException.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.archan.reflect.member.exception.common
-
-import kr.co.archan.reflect.global.exception.base.ProductException
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-class NoMemberException : ProductException("회원이 없습니다.")
