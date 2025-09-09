7d5c8777be23273c8e967fb502bce2f79e9d736e
박종하 <joshua_0621@naver.com>
2025-09-09 20:21:13 +0900

    [REMOVE] 미사용 함수 제거

--- src/main/kotlin/kr/co/archan/reflect/global/util/extension/Preconditions.kt
+++
@@ -1,7 +0,0 @@
-package kr.co.archan.reflect.global.util.extension
-
-import kr.co.archan.reflect.global.exception.base.ProductException
-
-inline fun requireAndThrowProductException(value: Boolean, ex: () -> ProductException) {
-    if (!value) throw ex()
-}
