76100d5bbea4d728c72d3a0b404f5c94159c6715
박종하 <joshua_0621@naver.com>
2025-09-09 20:16:55 +0900

    [FIX] 잘못된 입력값 예외 에러 스펙에 맞춰 수정

--- src/main/kotlin/kr/co/archan/reflect/global/exception/common/InvalidInputException.kt
+++ src/main/kotlin/kr/co/archan/reflect/global/exception/common/InvalidInputException.kt
@@ -1,9 +1,9 @@
 package kr.co.archan.reflect.global.exception.common
 
-import kr.co.archan.reflect.global.exception.base.ProductException
-import kr.co.archan.reflect.global.exception.types.InvalidInputField
+import kr.co.archan.reflect.global.exception.base.ApiException
+import kr.co.archan.reflect.global.exception.types.InvalidInputErrorSpec
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-class InvalidInputException(val field: InvalidInputField) : ProductException("잘못된 값을 입력하였습니다")
+class InvalidInputException(val specs: List<InvalidInputErrorSpec>) : ApiException(specs) {
+    override val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST
+}
