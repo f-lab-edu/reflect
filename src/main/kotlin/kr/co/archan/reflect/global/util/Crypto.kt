5232affb0e3634c3403ae8b528b4d6defd1f2151
박종하 <joshua_0621@naver.com>
2025-09-30 14:14:32 +0900

    [FIX] sha256 메서드 이름 변경 및 Mac을 이용한 해싱으로 수정

--- src/main/kotlin/kr/co/archan/reflect/global/util/Crypto.kt
+++ src/main/kotlin/kr/co/archan/reflect/global/util/Crypto.kt
@@ -1,11 +1,20 @@
 package kr.co.archan.reflect.global.util
 
-import java.security.MessageDigest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
-object Crypto {
-     fun sha256(s: String): String {
-        val md = MessageDigest.getInstance("SHA-256")
-        return Base64.getUrlEncoder().withoutPadding().encodeToString(md.digest(s.toByteArray()))
+@Component
+class Crypto(
+    @Value("\${security.sha.secret}")
+    private val secretKey: String
+) {
+    fun sha256WithNoSalt(s: String): String {
+        val mac = Mac.getInstance("HmacSHA256")
+        val secretKeySpec = SecretKeySpec(secretKey.toByteArray(), "HmacSHA256")
+        mac.init(secretKeySpec)
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(mac.doFinal(s.toByteArray()))
     }
 }
