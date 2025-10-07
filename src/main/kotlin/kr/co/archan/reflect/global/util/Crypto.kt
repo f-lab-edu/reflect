39d8591984b401791d7198e0247345a2ac5b2cec
박종하 <joshua_0621@naver.com>
2025-10-07 11:17:27 +0900

    [FEAT] 암호화 모듈에 비밀번호 해시 및 비교 함수 추가

--- src/main/kotlin/kr/co/archan/reflect/global/util/Crypto.kt
+++ src/main/kotlin/kr/co/archan/reflect/global/util/Crypto.kt
@@ -1,6 +1,7 @@
 package kr.co.archan.reflect.global.util
 
-import org.springframework.beans.factory.annotation.Value
+import kr.co.archan.reflect.global.properties.CryptoProperties
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import java.util.*
 import javax.crypto.Mac
@@ -8,13 +9,23 @@ import javax.crypto.spec.SecretKeySpec
 
 @Component
 class Crypto(
-    @Value("\${security.sha.secret}")
-    private val secretKey: String
+    private val cryptoProperties: CryptoProperties,
+    private val passwordEncoder: PasswordEncoder
 ) {
     fun sha256WithNoSalt(s: String): String {
         val mac = Mac.getInstance("HmacSHA256")
-        val secretKeySpec = SecretKeySpec(secretKey.toByteArray(), "HmacSHA256")
+        val secretKeySpec = SecretKeySpec(cryptoProperties.hashKey.toByteArray(), "HmacSHA256")
         mac.init(secretKeySpec)
         return Base64.getUrlEncoder().withoutPadding().encodeToString(mac.doFinal(s.toByteArray()))
     }
+
+    fun hashPassword(password: String): String {
+        return passwordEncoder.encode(password + cryptoProperties.pepperKey)
+    }
+
+    fun isPasswordMatches(storedHash: String, inputPassword: String): Boolean {
+        val material = inputPassword + cryptoProperties.pepperKey
+        return passwordEncoder.matches(material, storedHash)
+    }
+
 }
