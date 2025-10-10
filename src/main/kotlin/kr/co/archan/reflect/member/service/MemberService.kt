ffb7706d7f2f04bcda7fcca69ffdeb335faa7616
박종하 <joshua_0621@naver.com>
2025-10-10 21:16:07 +0900

    [FEAT] 회원가입 서비스 로직 추가

--- src/main/kotlin/kr/co/archan/reflect/member/service/MemberService.kt
+++ src/main/kotlin/kr/co/archan/reflect/member/service/MemberService.kt
@@ -1,6 +1,7 @@
 package kr.co.archan.reflect.member.service
 
 import kr.co.archan.reflect.member.domain.Member
+import kr.co.archan.reflect.member.exception.common.MemberAlreadyExistsException
 import kr.co.archan.reflect.member.exception.common.MemberNotFoundException
 import kr.co.archan.reflect.member.repository.MemberRepository
 import org.springframework.stereotype.Service
@@ -11,6 +12,16 @@ import org.springframework.transaction.annotation.Transactional
 class MemberService (
     private val memberRepository: MemberRepository
 ){
+
+    @Transactional(propagation = Propagation.MANDATORY)
+    fun saveNewMember(member: Member) : Member {
+
+        if (memberRepository.existsByEmailAndIsWithdrawnFalse(member.email)) {
+            throw MemberAlreadyExistsException()
+        }
+
+        return memberRepository.save(member)
+    }
 
     @Transactional(propagation = Propagation.REQUIRED)
     fun getMember(email: String) : Member {
