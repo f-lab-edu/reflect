3c148cf331a92cb0f4527faa684f12b8e8d8d238
박종하 <joshua_0621@naver.com>
2025-10-11 00:03:29 +0900

    [FEAT] AOP 사용 설정

--- src/main/kotlin/kr/co/archan/reflect/ReflectBeApplication.kt
+++ src/main/kotlin/kr/co/archan/reflect/ReflectBeApplication.kt
@@ -3,10 +3,12 @@ package kr.co.archan.reflect
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EnableAspectJAutoProxy
 @SpringBootApplication
 class ReflectBeApplication
 
